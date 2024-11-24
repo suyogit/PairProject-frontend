da1490e7140537adb02e9bd6bbd9685ba4ade5cf
Suyog Acharya <asuyog042@gmail.com>
2024-11-24 12:45:44 +0545

    edit profile

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,8 +1,14 @@
+import { useSelector } from "react-redux";
+import EditProfile from "./EditProfile";
 
 const Profile = () => {
+  const user = useSelector((store) => store.user);
   return (
-    <div>Profile</div>
-  )
-}
-
-export default Profile
+    user && (
+      <div>
+        <EditProfile user={user} />
+      </div>
+    )
+  );
+};
+export default Profile;
