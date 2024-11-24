eb5c1b3d8689cd003032981c04705c8127d5db57
Suyog Acharya <asuyog042@gmail.com>
2024-11-24 09:34:04 +0545

    user feed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,8 +1,40 @@
+import { useDispatch, useSelector } from "react-redux";
+import { BASE_URL } from "../utils/constants";
+import { addFeed } from "../utils/feedSlice";
+import axios from "axios";
+import { useEffect } from "react";
+import UserCard from "./UserCard";
 
 const Feed = () => {
-  return (
-    <div>Feed</div>
-  )
-}
+  const feed = useSelector((store) => store.feed);
+  const dispatch = useDispatch();
 
-export default Feed
+  const getFeed = async () => {
+    if (feed) {
+      return;
+    }
+    try {
+      const res = await axios.get(BASE_URL + "/feed", {
+        withCredentials: true,
+      });
+      console.log("res", res);
+      console.log("res?.data", res?.data);
+      console.log("res?.data?.data", res?.data?.data);
+      dispatch(addFeed(res?.data?.data));
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  useEffect(() => {
+    getFeed();
+  }, []);
+  return (
+    feed && (
+      <div className="flex justify-center my-10">
+        <UserCard user={feed[0]} />
+      </div>
+    )
+  );
+};
+
+export default Feed;
