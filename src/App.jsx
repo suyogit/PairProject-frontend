7681f584dbd4f6b0bb6d4b2b68848bbf7f9fd9d7
Suyog Acharya <asuyog042@gmail.com>
2024-11-27 16:45:57 +0545

    error page

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import { Provider } from "react-redux";
 import appStore from "./utils/appStore";
 import Connections from "./components/Connections";
 import Requests from "./components/Requests";
+import Error from "./components/Error";
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
               <Route path="/profile" element={<Profile />} />
               <Route path="/connections" element={<Connections />} />
               <Route path="/requests" element={<Requests />} />
+              <Route path="*" element={<Error />} />
             </Route>
           </Routes>
         </BrowserRouter>
