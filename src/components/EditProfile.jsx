8390f1fdb3fd7bdd0f0f57b353c5bc8ef9f068db
Suyog Acharya <asuyog042@gmail.com>
2024-11-27 12:57:45 +0545

    cloudinary feature

--- src/components/EditProfile.jsx
+++ src/components/EditProfile.jsx
@@ -4,27 +4,63 @@ import axios from "axios";
 import { BASE_URL } from "../utils/constants";
 import { useDispatch } from "react-redux";
 import { addUser } from "../utils/userSlice";
+import { ThreeDots } from "react-loader-spinner";
 
 const EditProfile = ({ user }) => {
   const [firstName, setFirstName] = useState(user.firstName);
   const [lastName, setLastName] = useState(user.lastName);
-  const [photoUrl, setPhotoUrl] = useState(user.photoUrl);
+  // const [photoUrl, setPhotoUrl] = useState(user.photoUrl);
   const [age, setAge] = useState(user.age || "");
   const [gender, setGender] = useState(user.gender || "");
   const [about, setAbout] = useState(user.about || "");
   const [error, setError] = useState("");
   const dispatch = useDispatch();
   const [showToast, setShowToast] = useState(false);
+  const [img, setImg] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [previewUrl, setPreviewUrl] = useState(user.photoUrl || "");
+
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    setImg(file);
+    if (file) {
+      const preview = URL.createObjectURL(file);
+      setPreviewUrl(preview); // Generate a preview URL
+    }
+  };
+
+  const uploadFile = async () => {
+    const data = new FormData();
+    data.append("file", img);
+    data.append("upload_preset", "images_preset");
+
+    try {
+      const cloudName = import.meta.env.VITE_APP_CLOUDINARY_CLOUD_NAME;
+      let resourceType = "image";
+      let api = `https://api.cloudinary.com/v1_1/${cloudName}/${resourceType}/upload`;
+
+      const res = await axios.post(api, data);
+      const { secure_url } = res.data; //https link
+      // console.log(secure_url);
+      return secure_url;
+    } catch (error) {
+      // console.error(error);
+    }
+  };
 
   const saveProfile = async () => {
     setError("");
+    setLoading(true);
+
     try {
+      const imgUrl = await uploadFile("image");
+
       const res = await axios.patch(
         BASE_URL + "/profile/edit",
         {
           firstName,
           lastName,
-          photoUrl,
+          photoUrl: imgUrl,
           age,
           gender,
           about,
@@ -32,6 +68,8 @@ const EditProfile = ({ user }) => {
         { withCredentials: true }
       );
       dispatch(addUser(res?.data?.data));
+      setImg(null);
+      setLoading(false);
       setShowToast(true);
       setTimeout(() => {
         setShowToast(false);
@@ -75,13 +113,36 @@ const EditProfile = ({ user }) => {
                   <div className="label">
                     <span className="label-text">Photo URL :</span>
                   </div>
-                  <input
+                  {/* <input
                     type="text"
                     value={photoUrl}
                     className="input input-bordered w-full max-w-xs"
                     onChange={(e) => setPhotoUrl(e.target.value)}
+                  /> */}
+
+                  <input
+                    type="file"
+                    accept="image/*"
+                    id="img"
+                    className="file-input file-input-bordered file-input-md w-full max-w-xs"
+                    onChange={handleFileChange}
                   />
                 </label>
+                <div className="card-actions justify-center m-2"> 
+                  {loading && (
+                    <ThreeDots
+                      height="60"
+                      width="60"
+                      radius="8"
+                      color="#4fa94d"
+                      ariaLabel="three-dots-loading"
+                      wrapperStyle={{}}
+                      wrapperClassName=""
+                      visible={true}
+                    />
+                  )}
+                </div>
+
                 <label className="form-control w-full max-w-xs my-2">
                   <div className="label">
                     <span className="label-text">Age:</span>
@@ -127,7 +188,14 @@ const EditProfile = ({ user }) => {
         </div>
         <div className="m-6 p-6">
           <UserCard
-            user={{ firstName, lastName, photoUrl, age, gender, about }}
+            user={{
+              firstName,
+              lastName,
+              photoUrl: previewUrl,
+              age,
+              gender,
+              about,
+            }}
           />
         </div>
       </div>
