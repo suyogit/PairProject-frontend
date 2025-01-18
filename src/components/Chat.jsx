b8b1a1ee8824c8723c3a073e9024b6b6631e8f2d
Suyog Acharya <asuyog042@gmail.com>
2025-01-18 19:06:47 +0545

    can send message

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -8,7 +8,7 @@ import { BASE_URL } from "../utils/constants";
 const Chat = () => {
   const { targetUserId } = useParams();
   const [messages, setMessages] = useState([{ text: "hellow" }]);
-  //   const [newMessage, setNewMessage] = useState("");
+  const [newMessage, setNewMessage] = useState("");
   const user = useSelector((store) => store.user);
   const userId = user?._id;
 
@@ -33,55 +33,39 @@ const Chat = () => {
   //     fetchChatMessages();
   //   }, []);
 
-  //   useEffect(() => {
-  //     if (!userId) {
-  //       return;
-  //     }
-  //     const socket = createSocketConnection();
-  //     // As soon as the page loaded, the socket connection is made and joinChat event is emitted
-  //     socket.emit("joinChat", {
-  //       firstName: user.firstName,
-  //       userId,
-  //       targetUserId,
-  //     });
-
-  //     socket.on("messageReceived", ({ firstName, lastName, text }) => {
-  //       console.log(firstName + " :  " + text);
-  //       setMessages((messages) => [...messages, { firstName, lastName, text }]);
-  //     });
-
-  //     return () => {
-  //       socket.disconnect();
-  //     };
-  //   }, [userId, targetUserId]);
-
-  //   const sendMessage = () => {
-  //     const socket = createSocketConnection();
-  //     socket.emit("sendMessage", {
-  //       firstName: user.firstName,
-  //       lastName: user.lastName,
-  //       userId,
-  //       targetUserId,
-  //       text: newMessage,
-  //     });
-  //     setNewMessage("");
-  //   };
-
   useEffect(() => {
     if (!userId) {
       return;
     }
     const socket = createSocketConnection();
+    // As soon as the page loaded, the socket connection is made and joinChat event is emitted
     socket.emit("joinChat", {
-      firstName: user?.firstName,
+      firstName: user.firstName,
       userId,
       targetUserId,
+    });
+
+    socket.on("messageReceived", ({ firstName, lastName, text }) => {
+      console.log(firstName + " :  " + text);
+      setMessages((messages) => [...messages, { firstName, lastName, text }]);
     });
 
     return () => {
       socket.disconnect();
     };
   }, [userId, targetUserId]);
+
+  const sendMessage = () => {
+    const socket = createSocketConnection();
+    socket.emit("sendMessage", {
+      firstName: user.firstName,
+      lastName: user.lastName,
+      userId,
+      targetUserId,
+      text: newMessage,
+    });
+    setNewMessage("");
+  };
 
   return (
     <div className="w-3/4 mx-auto border border-gray-600 m-5 h-[70vh] flex flex-col">
@@ -107,14 +91,14 @@ const Chat = () => {
         })}
       </div>
       <div className="p-5 border-t border-gray-600 flex items-center gap-2">
-        {/* <input
+        <input
           value={newMessage}
           onChange={(e) => setNewMessage(e.target.value)}
           className="flex-1 border border-gray-500 text-white rounded p-2"
-        ></input> */}
-        {/* <button onClick={sendMessage} className="btn btn-secondary">
+        ></input>
+        <button onClick={sendMessage} className="btn btn-secondary">
           Send
-        </button> */}
+        </button>
       </div>
     </div>
   );
