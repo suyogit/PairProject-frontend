cba25787b54a2b4c4eb24528ec26205cc1b19b86
Suyog Acharya <asuyog042@gmail.com>
2025-01-19 07:46:16 +0545

    changes made to work on render

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,2 @@
-// export const BASE_URL = "https://pairproject-backend-1.onrender.com";
-export const BASE_URL = "http://localhost:3000";
-
+export const BASE_URL = "https://pairproject-backend-1.onrender.com";
+// export const BASE_URL = "http://localhost:3000";
