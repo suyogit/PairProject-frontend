9767c42a10e8acac51ef85cd55a463d2b10f845b
Suyog Acharya <asuyog042@gmail.com>
2025-01-19 07:59:46 +0545

    trying to fix deployment issue

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -2,10 +2,10 @@ import io from "socket.io-client";
 import { BASE_URL } from "./constants";
 
 export const createSocketConnection = () => {
-  // return io(BASE_URL);
-  if (location.hostname === "localhost") {
-    return io(BASE_URL);
-  } else {
-    return io("/", { path: "/socket.io" });
-  }
+  return io(BASE_URL);
+  // if (location.hostname === "localhost") {
+  //   return io(BASE_URL);
+  // } else {
+  //   return io("/", { path: "/socket.io" });
+  // }
 };
